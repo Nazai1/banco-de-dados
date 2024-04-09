e78eed387f6da524d3cf28b43ba7cac3f7b20664
Nazai1 <136096140+Nazai1@users.noreply.github.com>
2024-04-09 17:57:44 -0300

    Add files via upload

---
+++ user_client.sql
@@ -0,0 +1,11 @@
+use empresa;
+insert into user (id,email,senha)
+values (1, 'rafabonilho@gmail.com',123);
+insert into user (id,email,senha)
+values (2,'raimundosertao@gmail.com',8524);
+insert into user (id,email,senha)
+values (3,'juliapereira@gmail.com.br','485Kj');
+
+select* from user;
+select email from user;
+
